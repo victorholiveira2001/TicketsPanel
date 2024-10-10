adb38c49e64db000e340084c42b727e616b5ed3a
victorholiveira2001 <victorholiveira2001+github@gmail.com>
2024-10-10 11:03:44 -0300

    Fix: Fixing Ticket's OpenTime bug

--- src/TicketsPanel/Migrations/20241010135028_OpenDataTicket.Designer.cs
+++ src/TicketsPanel/Migrations/20241010135028_OpenDataTicket.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TicketsPanel.Data;
 
@@ -11,9 +12,11 @@ using TicketsPanel.Data;
 namespace TicketsPanel.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241010135028_OpenDataTicket")]
+    partial class OpenDataTicket
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/TicketsPanel/Migrations/20241010135028_OpenDataTicket.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicketsPanel.Migrations
+{
+    /// <inheritdoc />
+    public partial class OpenDataTicket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OpenTime",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValueSql: "FORMAT(GETDATE(), 'dd/MM/yyyy HH:mm:ss')",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldDefaultValue: "02/10/2024 16:25:49");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OpenTime",
+                table: "Tickets",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "02/10/2024 16:25:49",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldDefaultValueSql: "FORMAT(GETDATE(), 'dd/MM/yyyy HH:mm:ss')");
+        }
+    }
+}
